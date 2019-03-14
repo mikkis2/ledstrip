c72ad2636f125ede849ac7262450bb5be0e84bcf
mikkis2 <48546421+mikkis2@users.noreply.github.com>
2019-03-14 09:55:03 +0200

    Add files via upload

---
+++ config.h
@@ -0,0 +1,50 @@
+/************************ Adafruit IO Config *******************************/
+
+// visit io.adafruit.com if you need to create an account,
+// or if you need your Adafruit IO key.
+#define IO_USERNAME    "migelinho"
+#define IO_KEY         "5fc99b472ebc44ed8e52ef44c8e244f9" 
+
+/******************************* WIFI **************************************/
+
+// the AdafruitIO_WiFi client will work with the following boards:
+//   - HUZZAH ESP8266 Breakout -> https://www.adafruit.com/products/2471
+//   - Feather HUZZAH ESP8266 -> https://www.adafruit.com/products/2821
+//   - Feather M0 WiFi -> https://www.adafruit.com/products/3010
+//   - Feather WICED -> https://www.adafruit.com/products/3056
+
+#define WIFI_SSID       "Smaragdi"
+#define WIFI_PASS       "aqP6l23Zk"
+//#define WIFI_SSID       "aalto open"
+//#define WIFI_PASS       ""
+//#define WIFI_SSID       "OnePlus3"
+//#define WIFI_PASS       "wemi1993"
+//#define WIFI_SSID       "majakka"
+//#define WIFI_PASS       "avarko friends wlan"
+
+
+// comment out the following two lines if you are using fona or ethernet
+#include "AdafruitIO_WiFi.h"
+AdafruitIO_WiFi io(IO_USERNAME, IO_KEY, WIFI_SSID, WIFI_PASS);
+
+
+/******************************* FONA **************************************/
+
+// the AdafruitIO_FONA client will work with the following boards:
+//   - Feather 32u4 FONA -> https://www.adafruit.com/product/3027
+
+// uncomment the following two lines for 32u4 FONA,
+// and comment out the AdafruitIO_WiFi client in the WIFI section
+// #include "AdafruitIO_FONA.h"
+// AdafruitIO_FONA io(IO_USERNAME, IO_KEY);
+
+
+/**************************** ETHERNET ************************************/
+
+// the AdafruitIO_Ethernet client will work with the following boards:
+//   - Ethernet FeatherWing -> https://www.adafruit.com/products/3201
+
+// uncomment the following two lines for ethernet,
+// and comment out the AdafruitIO_WiFi client in the WIFI section
+// #include "AdafruitIO_Ethernet.h"
+// AdafruitIO_Ethernet io(IO_USERNAME, IO_KEY);
---
+++ ledstrip_git.ino
@@ -0,0 +1,154 @@
+#define IO_USERNAME    "mig3linho"
+#define IO_KEY         
+#define WIFI_SSID      
+#define WIFI_PASS      
+
+#include "AdafruitIO_WiFi.h"
+AdafruitIO_WiFi io(IO_USERNAME, IO_KEY, WIFI_SSID, WIFI_PASS);
+#define greenPin 15
+#define bluePin  13
+#define redPin  12
+
+float r = 100;
+float g = 100;
+float b = 100;
+String hex;
+int brightness = 100;
+
+#include <AdafruitIO.h>
+#include <ArduinoHttpClient.h>
+
+
+// set up the 'ledcolor' feed
+AdafruitIO_Feed *ledcolor = io.feed("ledcolor");
+AdafruitIO_Feed *brightnes = io.feed("brightness");
+
+void setup() {
+  pinMode(greenPin, OUTPUT);
+  pinMode(bluePin, OUTPUT);
+  pinMode(redPin, OUTPUT); 
+// start the serial connection
+  Serial.begin(115200);
+  
+  //light up the led-strip to inform the device is on
+  analogWrite(redPin, r);
+  analogWrite(greenPin, g);
+  analogWrite(bluePin, b);
+  
+  
+  // wait for serial monitor to open
+  while(! Serial);
+ 
+  // connect to io.adafruit.com
+  Serial.print("Connecting to Adafruit IO");
+  io.connect();
+
+  ledcolor->onMessage(handleMessage);
+  brightnes->onMessage(handleBrightness);
+
+  // wait for a connection
+  while (io.status() < AIO_CONNECTED) {
+    Serial.println("Connecting...");
+  //blink the leds to inform the connection is being established  
+    analogWrite(redPin, 0);
+    analogWrite(greenPin, 0);
+    analogWrite(bluePin, 0);
+    delay(100);
+    
+    analogWrite(redPin, r);
+    analogWrite(greenPin, g);
+    analogWrite(bluePin, b);
+    delay(100);
+    
+    delay(300);
+  }
+
+  // we are connected
+  Serial.println();
+  Serial.println(io.statusText());
+  
+  //Set the leds to green to inform the connection is established
+    
+    analogWrite(redPin, 54);
+    analogWrite(greenPin, 251);
+    analogWrite(bluePin, 2);
+    delay(100);
+
+
+  
+  // set analogWrite range for ESP8266
+  #ifdef ESP8266
+    analogWriteRange(255);
+  #endif
+}
+
+// this function is called whenever a 'color' message
+// is received from Adafruit IO. it was attached to
+// the color feed in the setup() function above.
+void handleMessage(AdafruitIO_Data *data) {
+
+  r = data->toRed();
+  g = data->toGreen();
+  b = data->toBlue();
+  hex = data->value();
+ 
+  // print RGB values and hex value
+  Serial.println("Received:");
+  Serial.print("  - R: ");
+  Serial.println(r);
+  Serial.print("  - G: ");
+  Serial.println(g);
+  Serial.print("  - B: ");
+  Serial.println(b);
+  Serial.print("  - HEX: ");
+  Serial.println(hex);
+  
+
+  
+  // invert RGB values for common anode LEDs
+  analogWrite(redPin, r);
+  analogWrite(greenPin, g);
+  analogWrite(bluePin, b);
+ 
+}
+//this function is called whenever the brighness feed value is changed
+
+void handleBrightness(AdafruitIO_Data *data) {
+   
+  brightness = data->toInt();
+  
+  float jakaja = 100;
+  float a = brightness;
+
+  float kerroin = a / jakaja;
+  
+  float r2 = (r * kerroin);
+  float g2 = (g * kerroin);
+  float b2 = (b * kerroin);
+  
+  analogWrite(redPin, r2);
+  analogWrite(greenPin, g2);
+  analogWrite(bluePin, b2);
+  
+  Serial.println("handleBrightness:");
+  Serial.print("  - R: ");
+  Serial.println(r2);
+  Serial.print("  - G: ");
+  Serial.println(g2);
+  Serial.print("  - B: ");
+  Serial.println(b2);
+  Serial.print("  - HEX: ");
+  Serial.println(hex);
+  Serial.print("  - BRIGHTNESS: ");
+  Serial.println(brightness);
+}
+
+void loop() {  
+  
+  io.run();
+      
+} 
+
+
+
+
